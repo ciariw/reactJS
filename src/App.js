2f2c2f129a3f82dce93e289c4b5a8ba8053ea1e6
ciariw <90122269+ciariw@users.noreply.github.com>
2022-05-18 18:53:23 -0400

    Update App.js
    Moved Notes function. Minor changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import './css/bootstrap.css';
 import React ,{useState, useEffect, useRef} from 'react';
-
+import Notes from './Notes.js'
 
 function App() {
   return (
@@ -46,99 +46,33 @@ function About(){
     <h1>About page</h1>
     </>);
 }
-class ElementReferencer extends React.Component{
-  constructor(props){
-    super(props);
-    this.self = React.createRef();
 
-  }
 
-}
-function Lupdater(props){
-  // After I hit enter, I want to add another list item to the returned unordered list
-  //create a function that accepts the list and returns
-  // The reason why it executes twice is that it calls list update function
-  // I want to randomly generata a location that doesnt exist already
-  
-  function ListItemElement(props){ 
-    let [ElementState,ChangeElementState] = useState(props.position);
-    let [Clicked, ChangeClick] = useState(false);
-    //this element now has its own properties that I can send to the Drag Handler
-    const referens = useRef(null);
-    //by default, you havent clicked anything
-    const evl = (v) => {
-      console.log(Clicked)
-      if (Clicked){
-      console.log(Clicked)
-      ChangeElementState({top:v.clientY -v.target.offsetTop+50, left:v.clientX});
-      }
-    }
-    
-    return <span  ref = {referens}
-                  className = 'list-item'
-                  onClick={()=>{window.addEventListener('mousemove',evl,true);ChangeClick(!Clicked); console.log(Clicked)}}
-                  
-                  style={ElementState} 
-                  >{props.contents}</span>;
-  }
 
-  const notemap = props.lists.map((x,id)=><ListItemElement key = {id} contents={x} position={props.positionStates[id]}/>);
-  return notemap;
-}
-const Fun = ()=>{
-  const [note,UpdateNote] = useState('');
-  const [listitem,Listupdater] = useState([]);
-  const [positions, updatePos] = useState([]);
-
-  let templist = listitem.slice();
-  let tempcoord = positions.slice();
-
-  useEffect(()=>{
-    
-    window.addEventListener('keypress',(eventcode)=>{
-      if (eventcode.key === 'Enter'){
-        UpdateNote('');
-        if (note !== ''){
-          templist.push(note);
-          Listupdater(templist);
-          tempcoord.push({top:`${Math.random()* 45}em`, left:`${Math.random()* 95}%`});
-          updatePos(tempcoord);
-          }
-          
-    }})
-  }
-  )
-  return (
-    <div id="webContent">
-      <div id="post-widget">
-        
-          <input type="text" value = {note} onInput={(x) => {UpdateNote(x.target.value)}} ></input>
-          <span className='generic-button' 
-                onClick={console.log()}>Add</span>
-  
-      </div>
-      <div id="note-mapper">
-      <Lupdater lists={templist} positionStates={tempcoord} />
-      </div>
-      
-    </div>
-    );
-}
 const AppBody = ()=>{
   
   //let [state,UpdateState] = useState(["Home",<Home/>]);
-  let [state,UpdateState] = useState(["Fun",<Fun/>]);
+  let [state,UpdateState] = useState(["Home",<Home/>]);
   
   useEffect(x=>{document.title=state[0]},[state]);
   return (
           <>
             <div id="Nav-bar">
-              <ul>
+              <ul  id="Root-List-Items">
                 <li onClick={()=>{UpdateState(["Home",<Home/>])}}>Home</li>
                 <li onClick={()=>{UpdateState(["Print Quote",<Quote />])}}>Print Quote</li>
                 <li onClick={()=>{UpdateState(["Resume",<Resume/>])}}>Resume</li>
                 <li onClick={()=>{UpdateState(["About",<About/>])}}>About</li>
-                <li onClick={()=>{UpdateState(["Fun",<Fun/>])}}>Fun</li>
+                <li 
+                    onMouseOver={()=>console.log('wow, mouse over.')}
+                    className = "Dropdown">Apps
+                    <ul>
+                    <li onClick={()=>{UpdateState(["Notes",<Notes/>])}}>
+                      Notes App
+                    </li>
+                    <li>Wordle App</li>
+                    </ul>
+                </li>
               </ul>
             <SeachBar/>
             </div>
